bbd9f8ae1f0b96965f61ddce6de1bfc42636af7f
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-04-09 13:58:15 +0900

    Task機能を作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+  before_action :find_params, only: [:edit, :update, :show, :destroy]
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render "new"
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render "edit"
+    end
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+  def find_params
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content, :status, :limit, :user_id)
+  end
+
+
+end
