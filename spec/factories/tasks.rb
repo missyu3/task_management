cc88f0309280ae2dadd5b7d193d114b93dd7c819
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-04-15 13:31:11 +0900

    add Taskに対してのテストを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     title { "test1"}
     content { "testes1" }
-    status { 0 }
-    limit { 2000/01/01 }
+    status { 1 }
+    limit { "2024-05-23" }
   end
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let(:task_1) {task_1 = FactoryBot.create(:task, title: "test1" , content: "content1" , status: 2 , limit: "2024-05-23")}
   describe 'タスク一覧画面' do
+    context '一覧画面に登録したタスクが表示する時' do
+      before do
+        task_1
+        FactoryBot.create(:task, title: "test2" , content: "content2" , status: 3 , limit: "2025-08-12")
+        visit tasks_path
+      end
+      it "task1が一覧画面に表示されているか" do
+        expect(page).to have_content "test1"
+      end 
+      it "task2が一覧画面に表示されているか" do
+        expect(page).to have_content "test2"
+      end
+    end
+  end
+
+  describe '新規タスク登録画面' do
     context '新規タスク登録後、一覧画面に遷移される時' do
       before do
         visit new_task_path
@@ -30,7 +47,24 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
-  describe 'タスク一覧画面' do
+  describe 'タスク詳細画面' do
 
+    context '登録されているタスクの詳細画面に遷移後' do
+      before do
+        visit task_path(task_1.id)
+      end
+      it "詳細画面に選択されたタスクが表示されているか" do
+          expect(page).to have_content "test1"
+      end
+      it "詳細画面に選択されたコンテンツが表示されているか" do
+        expect(page).to have_content "content1"
+      end
+      it "詳細画面に選択された状態が表示されているか" do
+        expect(page).to have_content "完了"
+      end
+      it "詳細画面に終了期日が表示されているか" do
+        expect(page).to have_content "2024-05-23"
+      end
+    end
   end
 end
