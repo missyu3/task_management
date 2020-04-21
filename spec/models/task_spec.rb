992d6a2baa0f56d7d8b11c97949ebad2ff8ec688
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-04-21 17:26:01 +0900

    add modelテストにScopeを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,29 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it "titleが空ならバリデーションが通らない" do
+  it "【Validation】titleが空ならバリデーションが通らない" do
     task = FactoryBot.build(:task, title: "")
     expect(task).not_to be_valid
   end
-  it "contentが空ならバリデーションが通らない" do
+  it "【Validation】contentが空ならバリデーションが通らない" do
     task = FactoryBot.build(:task, content: "")
     expect(task).not_to be_valid
   end
-  it "created_atのOrderByが効いているか" do
+  it "【Scope】titleに部分一致の検索が行えているか" do
+    FactoryBot.create(:task, id: 1, title: "hoge")
+    FactoryBot.create(:task, id: 2, title: "test")
+    FactoryBot.create(:task, id: 3, title: "huga")
+    task1 = Task.all.title_include("ho")
+    task2 = Task.all.title_include("es")
+    task3 = Task.all.title_include("ga")
+    expect(task1.first.id).to eq 1
+    expect(task2.first.id).to eq 2
+    expect(task3.first.id).to eq 3
+  end
+  it "【Scope】statusの完全一致の検索が行えているか" do
+    FactoryBot.create(:task, id: 1, status: 0)
+    FactoryBot.create(:task, id: 2, status: 1)
+    FactoryBot.create(:task, id: 3, status: 2)
+    FactoryBot.create(:task, id: 4, status: 3)
+    task1 = Task.all.status_equal("2")
+    expect(task1.first.id).to eq 3
+  end
+  it "【Scope】created_atのOrderByが効いているか" do
     FactoryBot.create(:task, id: 1, created_at: Time.current + 1.days)
     FactoryBot.create(:task, id: 2, created_at: Time.current + 4.days)
     FactoryBot.create(:task, id: 3, created_at: Time.current + 2.days)
     FactoryBot.create(:task, id: 4, created_at: Time.current + 3.days)
-    expect(Task.all.order_by("created_at","DESC").map(&:id)).to eq [2,4,3,1]
+    expect(Task.all.created_before.map(&:id)).to eq [2,4,3,1]
   end
-  it "limitのOrderByが効いているか" do
+  it "【Method】limitのOrderByが効いているか" do
     FactoryBot.create(:task, id: 1, limit: Time.current + 1.days)
     FactoryBot.create(:task, id: 2, limit: Time.current + 4.days)
     FactoryBot.create(:task, id: 3, limit: Time.current + 2.days)
     FactoryBot.create(:task, id: 4, limit: Time.current + 3.days)
     expect(Task.all.order_by("limit","DESC").map(&:id)).to eq [2,4,3,1]
   end
-  it "引数がnilの時、created_atのOrderByが効いているか" do
+  it "【Method】order_byの引数がnilの時、created_atのOrderByが効いているか" do
     FactoryBot.create(:task, id: 1, created_at: Time.current + 1.days)
     FactoryBot.create(:task, id: 2, created_at: Time.current + 4.days)
     FactoryBot.create(:task, id: 3, created_at: Time.current + 2.days)
