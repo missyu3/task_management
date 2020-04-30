51a86bba8eb1bcd079681b1866429bc5d84211a1
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-04-30 14:59:51 +0900

    Add label機能を作成

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,2 +1,36 @@
 class LabelsController < ApplicationController
+  before_action :find_params, only: [:destroy]
+
+  def new
+    @label = Label.new
+  end
+
+  def create
+    label = Label.new(label_params)
+    if label.save
+      redirect_to labels_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @labels = Label.all
+  end
+
+  def destroy
+    @label.destroy
+    redirect_to labels_path
+  end
+
+  private
+
+  def label_params
+    params.require(:label).permit(:name)
+  end
+
+  def find_params
+    @label = Label.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :tasks
+  resources :labels, only: [:new, :create, :index, :destroy]
   resources :users
   namespace :admin do
     resources :users
